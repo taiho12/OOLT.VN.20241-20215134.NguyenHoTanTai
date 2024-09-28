529d90d9e99fe4340c5f655d642b0a9d91d62b4d
Ho Tai <taizka1234@gmail.com>
2024-09-28 20:57:44 +0700

    add

--- Lap01/src/Main/CaculateTwoNumbers.java
+++ Lap01/src/Main/CaculateTwoNumbers.java
@@ -7,16 +7,20 @@ public class CaculateTwoNumbers {
         String strNum1, strNum2; double num1, num2;
 
         strNum1 = JOptionPane.showInputDialog(null,
-            "Nguyen Ho Tan Tai - 20215134 - Please input the first number: ", "Input the first number", JOptionPane.INFORMATION_MESSAGE);
+            "Nguyen Ho Tan Tai - 20215134 - Please input the first number: ",
+            "Input the first number", JOptionPane.INFORMATION_MESSAGE);
         num1 = Double.parseDouble(strNum1);
 
         strNum2 = JOptionPane.showInputDialog(null,
-            "Nguyen Ho Tan Tai - 20215134 - Please input the second number: ", "Input the second number", JOptionPane.INFORMATION_MESSAGE);
+            "Nguyen Ho Tan Tai - 20215134 - Please input the second number: ",
+            "Input the second number", JOptionPane.INFORMATION_MESSAGE);
         num2 = Double.parseDouble(strNum2);
+
         if(num1/num2==0) {
             JOptionPane.showMessageDialog(null,"Loi chia cho 0");
             System.exit(0);
         }
+        
         JOptionPane.showMessageDialog(null,
            "Nguyen Ho Tan Tai - 20215134\n" +
                "The sum of those two numbers is:" + (num1 + num2) + "\n" +
