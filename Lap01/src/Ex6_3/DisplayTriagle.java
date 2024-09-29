a3e190de1b46ee9672df53c573d48ac140bffaa2
Ho Tai <taizka1234@gmail.com>
2024-09-29 22:02:58 +0700

    add

--- Lap01/src/Ex6_3/DisplayTriagle.java
+++ Lap01/src/Ex6_3/DisplayTriagle.java
@@ -1,19 +1,27 @@
 package Ex6_3;
 
+import java.util.Scanner;
+
+
 public class DisplayTriagle {
     public static void main(String[] args) {
-        int height = 5;
-        
-        for (int i = 1; i <= height; i++) {
-            for (int j = 1; j <= height - i; j++) {
-                System.out.print(" ");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter height of triagle: ");
+        try {
+            int heigth = sc.nextInt();
+
+            for (int i = 0; i < heigth; i++) {
+                for (int j = i; j < heigth; j++) {
+                    System.out.print(" ");
+                }
+                for (int j = 0; j < 2 * i + 1; j++) {
+                    System.out.print("*");
+                }
+                System.out.print("\n");
             }
-            
-            for (int k = 1; k <= 2 * i - 1; k++) {
-                System.out.print("*");
-            }
-            
-            System.out.println();
+        }catch (Exception e) {
+            System.out.println("Height of triagle  is not number can't print");
         }
+        sc.close();
     }
 }
